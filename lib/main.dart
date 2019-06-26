718a3a6cb0bea01bd9ee098a5ae5f72c20a90f20
gustavomm <gustavomm22.gm@gmail.com>
2019-06-26 11:59:47 -0300

    deslizar para excluir

--- lib/main.dart
+++ lib/main.dart
@@ -75,24 +75,37 @@ class _HomeState extends State<Home> {
                 child: ListView.builder(
                     padding: EdgeInsets.only(top: 10),
                     itemCount: _toDoList.length,
-                    itemBuilder: (context, index) {
-                      return CheckboxListTile(
-                        onChanged: (c){
-                          setState(() {
-                            _toDoList[index]["ok"] = c;
-                            _saveData();
-                          });
-                        },
-                        title: Text(_toDoList[index]["title"]),
-                        value: _toDoList[index]["ok"],
-                        secondary: CircleAvatar(
-                            child: Icon(_toDoList[index]["ok"]
-                                ? Icons.check
-                                : Icons.error)),
-                      );
-                    }))
+                    itemBuilder: buildItem))
           ],
         ));
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white)
+        )
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        onChanged: (c){
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["ok"]
+                ? Icons.check
+                : Icons.error)),
+      ),
+    );
   }
 
   Future<File> _getFile() async {
